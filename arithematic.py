831ad987acd7cdbc7e5e689a9f38c11c6e7f862e
12nisha12 <nishanisha72360@gmail.com>
2025-03-10 15:39:55 +0530

    Create arithematic.py

---
+++ arithematic.py
@@ -0,0 +1,10 @@
+#Arithmetic operations
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+print("the sum : ",num1+num2)
+print("the multiplication : ",num1*num2)
+print("the power : ",num1**num2)
+print("the subtraction : ",num1-num2)
+print("the division : ",num1/num2)
+print("the division : ",num1//num2)
+print("the division : ",num1%num2)
